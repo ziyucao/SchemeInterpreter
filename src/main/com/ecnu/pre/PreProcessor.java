662e485c6b1fe61bcd344b74c1f432bb60a09da7
ddl <ddl19971129@163.com>
2018-05-30 22:01:44 +0800

    preprocessor部分修改和preprocessor测试类

--- src/main/com/ecnu/pre/PreProcessor.java
+++ src/main/com/ecnu/pre/PreProcessor.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 
 public class PreProcessor {
 
-    public static ArrayList preProcessor(String input)
+    public static ArrayList preProcess(String input)
     {
-        //deal with comments
-        String noComment = input.split(";")[0];
-        if (noComment == null || noComment.equals("")) {
+        if (isEmpty(input))
+        {
             return new ArrayList();
         }
 
+        //deal with comments
+        String noComment = input.split(";")[0];
+
         //deal with "(" and ")" and split to tokens
+        if (isEmpty(noComment)) {
+            return new ArrayList();
+        }
         String replaceLeftParenthese = noComment.replaceAll("\\(", " ( ");
         String replaceRightParenthese = replaceLeftParenthese.replaceAll("\\)", " ) ");
         String[] tokens = replaceRightParenthese.split("\n|\r|\t| ");
@@ -20,13 +25,23 @@ public class PreProcessor {
         //delete empty strings in "tokens"
         ArrayList result = new ArrayList();
         for(String token : tokens) {
-            if (token == null || !token.equals(""))
+            if (!isEmpty(token))
             {
                 result.add(token);
             }
         }
 
         return result;
+    }
+
+    private static boolean isEmpty(String input)
+    {
+        if (input == null || input.isEmpty()) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 }
