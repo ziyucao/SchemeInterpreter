d2c826fffc43639c0dae518c816c510ee7ddea4d
ddl <ddl19971129@163.com>
2018-05-30 18:30:08 +0800

    preprocessor

---
+++ src/com/ecnu/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String a = "a\nb\rc d";
+        String[] token = a.split("\n|\r| ");
+        System.out.println(token[2]);
+        // System.out.println("Hello World!");
+    }
+}
